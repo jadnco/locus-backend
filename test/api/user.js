603e837a7332803cceffa1875a88f07b414fe776
Jaden Dessureault <jaden.dessureault@gmail.com>
2016-02-11 09:30:56 -0600

    Ignore babel modules with Flow

--- test/api/user.js
+++ test/api/user.js
@@ -8,13 +8,13 @@ const route = `http://${IP.address()}:1998/api/users`;
 
 describe('User Model', () => {
   describe('GET single user', () => {
-    it('should have content-type of json', done => {
-      let options = {
-        uri: `${route}/56b95ffa9a663798f7c9833`,
-        method: 'GET',
-        resolveWithFullResponse: true,
-      };
+    let options = {
+      uri: `${route}/56b95ffa9a663798f7c9833`,
+      method: 'GET',
+      resolveWithFullResponse: true,
+    };
 
+    it('should have content-type of json', done => {
       Request(options)
         .then(res => {
           expect(res.headers['content-type']).to.contain('application/json');
